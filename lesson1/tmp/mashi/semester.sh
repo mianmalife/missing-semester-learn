719357ad22873625f8bbce8972fb6933655629f3
skea <253590433@qq.com>
2023-01-05 11:55:17 +0800

    Delete lesson1/tmp directory

--- lesson1/tmp/mashi/semester.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p tmp
-cd tmp/
-mkdir -p mashi
-cd mashi/
-touch semester.sh
-# echo -e '#!/bin/sh\ncurl --head --silent https://missing.csail.mit.edu' > semester.sh
-echo '#!/bin/sh' > semester.sh
-echo curl --head --silent https://missing.csail.mit.edu >> semester.sh
